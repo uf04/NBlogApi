a2b0d121930c39bb13d0f9c694c163e8c866b564
uf04 <yeosch123456789@gmail.com>
2025-05-19 07:34:35 +0000

    blog 함수

---
+++ main.py
@@ -0,0 +1,9 @@
+# blog.py 에서 함수 만들어서 모듈 포함시켜서 활용
+import blog
+print("네이버 맛집 검색")
+
+# 여기에서 메인 프로그램 작성
+# 맛집 검색: 여기 맛집 입력하면 blog 함수로 가서 결과가
+search = input("맛집 검색: ")
+# 결과가 나오는 프로그램 만들기
+blog.nblog(search)
